dca7dc832440aca94865a669dee5a37fdfa9d84f
jr1412594 <jr.medina1412@gmail.com>
2020-11-27 20:03:41 -0700

    todo container and todo items on page

--- src/App.js
+++ src/App.js
@@ -1,12 +1,29 @@
-import logo from './logo.svg';
+import { Component } from 'react'
 import './App.css';
+import TodoContainer from './components/TodoContainer'
+const baseURL = "http://localhost:7000"
+const todosURL = `${baseURL}/todos`
 
-function App() {
-  return (
-    <div className="App">
-    
-    </div>
-  );
+class App extends Component {
+  state = {
+    todos: []
+  }
+  componentDidMount(){
+    this.getTodos()
+  }
+getTodos = () => {
+  fetch(todosURL)
+  .then(response => response.json())
+  .then(todos => this.setState({todos}))
+}
+  render(){
+    return (
+      <div className="App">
+        <h1> Todo App </h1>
+        <TodoContainer todos={this.state.todos}/>
+      </div>
+    );
+  }
 }
 
 export default App;
